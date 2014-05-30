2725a3376bcb30b1cad0637ea4dd6a3c870f7926
masbicudo <masbicudo@gmail.com>
2014-05-29 21:20:38 -0300

    Project updates... and some .Net tests.

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        [StructLayout(LayoutKind.Explicit, Size = 0, Pack = 0, CharSet = CharSet.Ansi)]
+        struct VOID { }
+
+        static unsafe void Main(string[] args)
+        {
+            Console.WriteLine(string.Format("{0} = {1}", "sizeof(VOID)", sizeof(VOID)));
+            foreach (var eachAttr in typeof(VOID).GetCustomAttributes(true))
+                Console.WriteLine(string.Format("{0} = {1}", "eachAttr.GetType().Name", eachAttr.GetType().Name));
+            Console.ReadKey();
+        }
+    }
+}
