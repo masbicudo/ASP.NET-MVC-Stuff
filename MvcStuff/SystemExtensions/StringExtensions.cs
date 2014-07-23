88e6e77ae5577eff413fde98417a19a38ed1ce47
masbicudo <masbicudo@gmail.com>
2014-07-23 12:34:50 -0300

    StringExtensions (needed for previous commit of AcceptsJsonResponse)

---
+++ MvcStuff/SystemExtensions/StringExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+
+namespace MvcStuff.SystemExtensions
+{
+    internal static class StringExtensions
+    {
+        public static IEnumerable<string> EnumerableSplit(
+            [NotNull] this string str,
+            char separator,
+            StringSplitOptions options = StringSplitOptions.None)
+        {
+            if (str == null)
+                throw new ArgumentNullException("str");
+
+            return EnumerableSplitInternal(str, separator, options);
+        }
+
+        private static IEnumerable<string> EnumerableSplitInternal(
+            [NotNull] string str,
+            char separator,
+            StringSplitOptions options)
+        {
+            var prevPos = 0;
+            while (true)
+            {
+                var nextPos = str.IndexOf(separator, prevPos);
+
+                if (nextPos < 0)
+                    yield break;
+
+                if (options != StringSplitOptions.RemoveEmptyEntries || nextPos != prevPos)
+                    yield return str.Substring(prevPos, nextPos - prevPos);
+
+                prevPos = nextPos + 1;
+            }
+        }
+    }
+}
